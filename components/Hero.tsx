9259a453d04e9ba421fa5deea5b2b9a5aded7a4a
vvel0x <issa4700@gmail.com>
2022-09-04 17:50:14 +0100

    ✨ Hero text and image now fades-in on load.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import ExportedImage from "next-image-export-optimizer";
+import { useState, useEffect } from "react";
 import Container from "./Container";
 import Nav from "./Nav";
 
@@ -8,21 +9,38 @@ type Header = {
 };
 
 const Hero: React.FC<Header> = ({ bgImage, children }) => {
+  const [imageHasLoaded, setImageHasLoaded] = useState(false);
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    setFadeIn(true);
+  }, []);
+
   return (
     <header className="relative bg-indigo-dye">
       <figure className="absolute inset-0 z-0">
         {bgImage && (
           <ExportedImage
             src={bgImage}
+            onLoad={() => setImageHasLoaded(true)}
             layout="fill"
-            className="object-cover"
+            className={`object-cover transition duration-1000 ${
+              imageHasLoaded ? "opacity-100" : "opacity-0 "
+            }`}
+            loading="eager"
             alt="Hero Image"
           />
         )}
       </figure>
       <div className="relative bg-gradient-to-r from-black/90 lg:from-black/80  to-black/20 ">
         <Nav />
-        <Container className="py-10">{children}</Container>
+        <Container
+          className={`py-10 duration-700 delay-50 ${
+            fadeIn ? "opacity-100 translate-y-0" : "opacity-0 translate-y-1"
+          } `}
+        >
+          {children}
+        </Container>
       </div>
     </header>
   );
