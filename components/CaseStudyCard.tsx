dc0a62adf48e01e829bd9b95419b3cd1d4aaa985
issa4700 <issa4700@gmail.com>
2022-11-24 12:41:26 +0800

    Added case studies

--- components/CaseStudyCard.tsx
+++ components/CaseStudyCard.tsx
@@ -1,4 +1,5 @@
 import { MapPinIcon } from "@heroicons/react/20/solid";
+import { BanknotesIcon, CalendarIcon } from "@heroicons/react/24/solid";
 import ExportedImage from "next-image-export-optimizer";
 
 interface CaseStudy {
@@ -7,12 +8,13 @@ interface CaseStudy {
   thumbnail: string;
   size: number;
   whitepaper?: string;
+  year?: number;
 }
 
 export const CaseStudyCard: React.FC<{ data: CaseStudy }> = ({ data }) => {
   const { type, title, thumbnail, size, whitepaper = "#" } = data;
   return (
-    <a href={whitepaper} className="group">
+    <a href={whitepaper} className="group cursor-default">
       <article className="flex flex-col gap-2">
         <figure className="relative aspect-[3/2]">
           <ExportedImage
@@ -40,36 +42,40 @@ export const CaseStudyCard: React.FC<{ data: CaseStudy }> = ({ data }) => {
 export const CaseStudyCardVariant: React.FC<{ data: CaseStudy }> = ({
   data,
 }) => {
-  const { type, title, thumbnail, size, whitepaper = "#" } = data;
+  const { type, title, thumbnail, size, whitepaper = "#", year } = data;
   return (
-    <a href={whitepaper} className="group">
-      <article className="flex flex-col gap-2">
-        <figure className="relative aspect-[3/2]">
-          <ExportedImage
-            layout="fill"
-            src={thumbnail}
-            alt=""
-            className="absolute inset-0 bg-slate-300 object-cover w-full h-full"
-          />
-          <div className="relative h-full bg-transparent group-hover:bg-slate-200 group-hover:bg-opacity-25"></div>
-        </figure>
-        <div className="flex justify-between items-center text-xs text-gray-700">
-          <span className="bg-gray-300 px-3 py-1 rounded-full uppercase tracking-wide">
-            {type}
-          </span>
-          <div className="flex gap-1 items-center font-semibold text-gray-600">
-            <span>{size.toFixed(2)} kWp</span>
-            <span>|</span>
-            <span className="flex items-center gap-1">
-              <MapPinIcon className="h-3 w-3" /> Kuala Lumpur
-            </span>
-          </div>
+    // <a href={whitepaper} className="group cursor-default">
+    <article className="group flex flex-col gap-2">
+      <figure className="relative aspect-[3/2]">
+        <ExportedImage
+          layout="fill"
+          src={thumbnail}
+          alt=""
+          className="absolute inset-0 bg-slate-300 object-cover w-full h-full"
+        />
+        <div className="relative h-full bg-transparent group-hover:bg-slate-200 group-hover:bg-opacity-25"></div>
+      </figure>
+      <div className="flex justify-between items-center text-xs text-gray-700">
+        <span className="bg-gray-300 px-3 py-1 rounded-full uppercase tracking-wide">
+          {type}
+        </span>
+        <div className="flex gap-1 items-center font-semibold text-gray-600">
+          <span>{size.toFixed(2)} kWp</span>
+          {year ? (
+            <>
+              <span>|</span>
+              <span className="flex items-center gap-1">
+                <CalendarIcon className="h-3 w-3" /> {year}
+              </span>
+            </>
+          ) : null}
         </div>
-        <h1 className="group-hover:text-indigo-dye font-semibold first-letter:uppercase">
-          {title}
-        </h1>
-      </article>
-    </a>
+      </div>
+      <h1 className="group-hover:text-indigo-dye font-semibold first-letter:uppercase">
+        {title}
+      </h1>
+    </article>
+    // </a>
   );
 };
 
