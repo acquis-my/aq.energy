6a2fd5931de3bc97cef8830a525c0ef43df22342
issa4700 <issa4700@gmail.com>
2023-05-23 12:12:41 +0800

    fix: export optimized images

--- next.config.js
+++ next.config.js
@@ -7,12 +7,14 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
+  transpilePackages: ["next-image-export-optimizer"],
   env: {
     nextImageExportOptimizer_imageFolderPath: "public/images",
     nextImageExportOptimizer_exportFolderPath: "out",
     nextImageExportOptimizer_quality: 80,
     nextImageExportOptimizer_storePicturesInWEBP: true,
     nextImageExportOptimizer_generateAndUseBlurImages: false,
+    nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
   },
 };
 
