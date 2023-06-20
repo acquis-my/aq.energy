0282f4884455293cf98d4626dab9d478d0d7b400
Issa <vvel0x@users.noreply.github.com>
2023-06-20 14:11:58 +0800

    fix case studies not loading

--- src/app/case-studies/CaseGrid.tsx
+++ src/app/case-studies/CaseGrid.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import Container from "~/components/Container";
-// import caseStudies from "~/_content/studies.json";
 import { studies as caseStudies } from "~/_content";
 import { CaseStudyCardVariant } from "~/components/CaseStudyCard";
 
