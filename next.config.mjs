3755bd0af1553d825f6360cebb1204f02cc1b096
Issa <vvel0x@users.noreply.github.com>
2023-06-22 17:31:32 +0800

    temp: remove env validation

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+};
+
+export default nextConfig;
