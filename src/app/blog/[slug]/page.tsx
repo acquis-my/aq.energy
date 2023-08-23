ba3f2256846a410566687a6a3b9c10cf0c7e5630
Issa <44219189+vvel0x@users.noreply.github.com>
2023-08-23 02:22:09 +0000

    fix(blog): text illegible on browser with dark mode on

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -43,12 +43,12 @@ export default async function PostPage({ params }: PostPageProps) {
   const updatedAt = post._updatedAt ? new Date(post._updatedAt) : null;
 
   return (
-    <article className="max-w-screen-md mx-auto py-12 w-full leading-7">
+    <article className="mx-auto w-full max-w-screen-md py-12 leading-7">
       <time
         dateTime={publishedAt.toISOString()}
-        className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
+        className="order-first flex items-center text-base text-zinc-500 "
       >
-        <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
+        <span className="h-4 w-0.5 rounded-full bg-zinc-300"></span>
         <span className="ml-3 font-medium">Published</span>
         <span className="ml-2">
           {Intl.DateTimeFormat("en-GB", {
@@ -58,20 +58,20 @@ export default async function PostPage({ params }: PostPageProps) {
           }).format(publishedAt)}
         </span>
       </time>
-      <h2 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+      <h2 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl">
         {post.title}
       </h2>
 
-      <figure className="relative aspect-[3/2] sm:aspect-video mt-10 mb-14 bx-container-padding">
+      <figure className="bx-container-padding relative mb-14 mt-10 aspect-[3/2] sm:aspect-video">
         <Image
-          className="object-cover h-full w-full text-center sm:rounded-xl bx-container bg-slate-100"
+          className="bx-container h-full w-full bg-slate-100 object-cover text-center sm:rounded-xl"
           src={post.coverImage}
           alt={post.title}
           fill
         />
       </figure>
 
-      <div className="font-default max-w-screen-sm mx-auto prose">
+      <div className="prose mx-auto max-w-screen-sm font-default">
         <PortableText
           value={post.content}
           components={{
@@ -81,7 +81,7 @@ export default async function PostPage({ params }: PostPageProps) {
       </div>
 
       {updatedAt ? (
-        <footer className="text-sm text-zinc-400 border-t mt-12 py-4">
+        <footer className="mt-12 border-t py-4 text-sm text-zinc-500">
           <time dateTime={updatedAt.toISOString()}>
             <span>Last Updated</span>
             <span className="ml-2">
