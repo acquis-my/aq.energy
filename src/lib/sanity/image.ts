1588a58f7fbd5ca2c7b6f90629e77ecdad00865c
Issa <vvel0x@users.noreply.github.com>
2023-10-19 05:07:53 +0000

    fix: images in blog posts with incorrect aspect ratios

--- src/lib/sanity/image.ts
+++ src/lib/sanity/image.ts
@@ -1,4 +1,5 @@
 import createImageUrlBuilder from "@sanity/image-url";
+import { z } from "zod";
 
 import { env } from "~/env.mjs";
 
@@ -7,17 +8,45 @@ const imageBuilder = createImageUrlBuilder({
   dataset: env.SANITY_DATASET,
 });
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const urlForImage = (source: any) => {
-  // Ensure that source image contains a valid reference
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  if (!source?.asset?._ref) {
-    return "";
-  }
+const imageSchema = z.object({
+  asset: z.object({
+    _ref: z.string(),
+  }),
+});
 
-  return imageBuilder
-    ?.image(source as string)
-    .auto("format")
-    .fit("max")
-    .url();
+function imageSource(source: unknown) {
+  const result = imageSchema.safeParse(source);
+
+  if (!result.success) return null;
+  return result.data;
+}
+
+/**
+ * Returns the dimensions of an image asset from Sanity CMS.
+ * @param source - The image asset source.
+ * @returns An array containing the width and height of the image asset.
+ */
+export const getImgDimensions = (source: unknown) => {
+  const assetRef = imageSource(source);
+  const dimRegex = /([0-9]+)x([0-9]+)/;
+  const dimensions = assetRef ? assetRef.asset._ref.match(dimRegex) : null;
+
+  if (!dimensions) return { width: 0, height: 0 };
+
+  return {
+    width: parseInt(dimensions[1]!),
+    height: parseInt(dimensions[2]!),
+  };
+};
+
+/**
+ * Returns the URL of  image from Sanity CMS.
+ * @param source - The source of the image.
+ * @returns The URL of the optimized image.
+ */
+export const urlForImage: (source: unknown) => string = (source) => {
+  const assetRef = imageSource(source);
+
+  if (!assetRef) return "";
+  return imageBuilder.image(assetRef).auto("format").fit("max").url();
 };
