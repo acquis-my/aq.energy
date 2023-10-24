4701a6a2b68eef01ffba7b47083b874d2557dc1f
Issa <vvel0x@users.noreply.github.com>
2023-10-24 06:38:51 +0000

    add(sitemap): page priority

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,26 +2,56 @@ import { type MetadataRoute } from "next";
 import { getPosts } from "~/lib/data";
 import { navLinks } from "~/links";
 
+type Sitemap = MetadataRoute.Sitemap;
+
 const SITE_DOMAIN = "https://www.aq.energy" as const;
+const excludedPaths = new Set(["/blog"]);
 
 function withDomain(paths: TemplateStringsArray, ...values: string[]) {
   return `${SITE_DOMAIN}${String.raw(paths, ...values)}`;
 }
 
-export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
-  const mainPages = navLinks.map((link) => link.dest);
-
-  const links = ["/", "/quote", "/faq", ...mainPages].map((url) => ({
-    url: withDomain`${url}`,
-    lastModified: new Date(),
-  }));
+export default async function Sitemap(): Promise<Sitemap> {
+  const mainPages = navLinks
+    .filter((l) => !excludedPaths.has(l.dest))
+    .map((link) => ({
+      url: withDomain`${link.dest}`,
+      lastModified: new Date(),
+      priority: 0.8,
+    }));
 
   const blogPosts = await getPosts().then((r) =>
     r.map(({ slug, _updatedAt: updatedAt }) => ({
       url: withDomain`/blog/${slug}`,
       lastModified: updatedAt ? new Date(updatedAt) : undefined,
+      priority: 0.64,
     }))
   );
 
-  return [...links, ...blogPosts];
+  const links = [
+    {
+      url: withDomain`/`,
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: withDomain`/quote`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: withDomain`/blog`,
+      lastModified: new Date(),
+      priority: 0.64,
+    },
+    {
+      url: withDomain`/faq`,
+      lastModified: new Date(),
+      priority: 0.64,
+    },
+    ...mainPages,
+    ...blogPosts,
+  ].sort((a, b) => b.priority - a.priority) satisfies Sitemap;
+
+  return links;
 }
