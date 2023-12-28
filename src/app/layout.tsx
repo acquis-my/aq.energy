e7c89003d4c0458b53b243214be33ec2c5e86152
Issa <vvel0x@users.noreply.github.com>
2023-12-28 03:06:04 +0000

    Closes AQE-11

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   title: "AQ Energy",
 };
 
-export const revalidate = 300;
+export const revalidate = 60 * 30;
 
 export default function RootLayout({
   // Layouts must accept a children prop.
