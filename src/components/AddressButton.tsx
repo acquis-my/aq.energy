eaa622e96aba43dab11d4f12d0ccce07df638b87
Tan Jun Yan <yan@yan3321.com>
2024-11-05 10:51:25 +0800

    style: use props directly in address button

--- src/components/AddressButton.tsx
+++ src/components/AddressButton.tsx
@@ -1,27 +1,25 @@
 import { MapPinIcon } from "@heroicons/react/24/solid";
 import Icon from "./Icon";
 
-export const AddressButton = ({
-  name,
-  mapLink,
-  address,
-}: {
+interface AddressButtonProps {
   name: string;
   mapLink: string;
   address: string;
-}) => {
+}
+
+export const AddressButton = (props: AddressButtonProps) => {
   return (
     <a
-      href={mapLink}
+      href={props.mapLink}
       className="items-left group flex flex-col gap-2 rounded-md border p-4 text-black-coral transition ease-in-out hover:bg-indigo-dye hover:text-white"
     >
       <div className="flex items-center gap-3">
         <Icon className="transition group-hover:bg-indigo-dye">
           <MapPinIcon className="h-6 w-6 transition group-hover:fill-cyber-yellow" />
         </Icon>
-        <span className="font-semibold">{name}</span>
+        <span className="font-semibold">{props.name}</span>
       </div>
-      <span>{address}</span>
+      <span>{props.address}</span>
     </a>
   );
 };
